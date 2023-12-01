2d6f6a5309db8ed28b4b96793791c9f7a1c8b009
Duncan Barnes <90353137+dbar2002@users.noreply.github.com>
2023-12-01 08:40:18 -0800

    spam detector operates based off of keywords subject to change

--- src/SpamDetector.java
+++ src/SpamDetector.java
@@ -16,16 +16,23 @@ public class SpamDetector {
     }
 
     private static boolean isSpam(String message) {
-        // Simple rules for demonstration purposes
-        // You can enhance and customize these rules based on your research and requirements
+        // Common spam keywords
+        String[] spamKeywords = {
+                "100% more", "100% free", "100% satisfied", "additional income", "be your own boss",
+                "best price", "big bucks", "billion", "cash bonus", "cents on the dollar",
+                "consolidate debt", "double your cash", "double your income", "earn extra cash",
+                "earn money", "eliminate bad credit", "extra cash", "extra income", "expect to earn",
+                "fast cash", "financial freedom", "free access", "free consultation", "free gift",
+                "free hosting", "free info", "free investment", "free membership", "free money",
+                "free preview", "free quote", "free trial", "full refund", "get out of debt",
+                "get paid", "giveaway", "guaranteed", "increase sales", "increase traffic",
+                "incredible deal", "lower rates", "lowest price", "make money", "million dollars",
+                "miracle", "money back", "once in a lifetime", "one time", "pennies a day",
+                "potential earnings", "prize", "promise", "pure profit", "risk-free",
+                "satisfaction guaranteed", "save big money", "save up to", "special promotion"
+        };
 
-        // Rule 1: Check for excessive use of capital letters
-        if (countUpperCaseLetters(message) > 10) {
-            return true;
-        }
-
-        // Rule 2: Check for the presence of specific spam keywords
-        String[] spamKeywords = {"earn money", "lose weight", "free", "click here"};
+        // Check if the message contains any spam keywords
         for (String keyword : spamKeywords) {
             if (message.toLowerCase().contains(keyword)) {
                 return true;
@@ -33,15 +40,5 @@ public class SpamDetector {
         }
 
         return false;
-    }
-
-    private static int countUpperCaseLetters(String text) {
-        int count = 0;
-        for (char c : text.toCharArray()) {
-            if (Character.isUpperCase(c)) {
-                count++;
-            }
-        }
-        return count;
     }
 }
